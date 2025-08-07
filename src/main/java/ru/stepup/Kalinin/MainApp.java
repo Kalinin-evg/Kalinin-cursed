28db85fe9dfc069c1e099e126238ee3ef373200c
Kalinin Evgeniy <kalinin.gdn@gmail.com>
2025-08-07 16:29:46 +0300

    Дороботка логики проекта.

--- src/main/java/ru/stepup/Kalinin/MainApp.java
+++ src/main/java/ru/stepup/Kalinin/MainApp.java
@@ -3,9 +3,9 @@ package ru.stepup.Kalinin;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("12");
-        System.out.println("1");
-        System.out.println("Hello world");
+        System.out.println("162");
+        System.out.println("71");
+        System.out.println("Hello _world");
     }
 }
 
