630be1d61d6ab260313380f40e48e02a83bfb245
Kalinin Evgeniy <kalinin.gdn@gmail.com>
2025-08-07 15:43:06 +0300

    initial commit

---
+++ src/main/java/ru/stepup/Kalinin/MainApp.java
@@ -0,0 +1,10 @@
+package ru.stepup.Kalinin;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+    }
+}
+
